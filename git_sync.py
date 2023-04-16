0994571038951db7f99900900e9a4db30b692db3
calvin <calvinducharme@gmail.com>
2023-04-16 00:05:26 -0700

    updated

--- git_sync.py
+++ git_sync.py
@@ -3,6 +3,7 @@ import os
 paths = [
     r"C:\Users\calvi\3D Objects\my_files",
     r"C:\Users\calvi\3D Objects\firebase-app\public",
+    r"C:\Users\calvi\3D Objects\git_sync",
 ]
 
 for item in paths:
