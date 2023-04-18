7b730e5e0b402f2c3d14b5f7879652af54cefdee
Calvin <calvinducharme@gmail.com>
2023-04-18 10:37:07 -0700

    Test

--- git_sync.py
+++ git_sync.py
@@ -1,11 +1,17 @@
 import os
 
-paths = [
-    r"C:\Users\calvi\3D Objects\my_files",
-    r"C:\Users\calvi\3D Objects\firebase-app\public",
-    r"C:\Users\calvi\3D Objects\git_sync",
-]
+try: 
+    log_path = r"C:\Users\calvi\3D Objects\git_sync\log.txt"
+    paths = [
+        r"C:\Users\calvi\3D Objects\my_files",
+        r"C:\Users\calvi\3D Objects\firebase-app\public",
+        r"C:\Users\calvi\3D Objects\git_sync",
+    ]
 
-for path in paths:
-    cmd = 'cd "'+ path + '" & git pull'
-    os.system(cmd)
+    for path in paths:
+        cmd = 'cd "'+ path + '" & git pull > "'+log_path+'"'
+        os.system(cmd)
+
+except Exception as e:
+    print("Something happened: "+e)
+    raise
